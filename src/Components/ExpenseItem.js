709f3642309989d4895e037f329820b55c3e4797
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-03-27 00:07:17 +0530

    useState in react

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -4,12 +4,9 @@ import React,{ useState } from 'react';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  // const [title,setTitle]=useState(props.title);
   const [amount,setamount]=useState(props.amount);
     function clickHandler(){
-        // setTitle("Update!");
         setamount(100);
-        // console.log(title);
     }
   return (
     <Card className='expense-item'>
