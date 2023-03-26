83b2a7dca2aa7b427120645ed25626463a1f72a1
Utkarsh Badoni <utkarshbadoni2000@gmail.com>
2023-03-27 00:01:27 +0530

    use state changes

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,22 @@
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
+import React,{ useState } from 'react';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  // const [title,setTitle]=useState(props.title);
+  const [amount,setamount]=useState(props.amount);
     function clickHandler(){
-        console.log("Delete Expense");
+        // setTitle("Update!");
+        setamount(100);
+        // console.log(title);
     }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>${amount}</div>
       </div>
       <button onClick={clickHandler}>Delete Expense</button>
     </Card>
